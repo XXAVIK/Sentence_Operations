2f755e3a0fd59df30948015db154a667af96d2e2
XXAVIK <ddg1377@gmail.com>
2023-11-14 19:56:47 +0300

    Добавьте файлы проекта.

---
+++ Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string phraseInput = Console.ReadLine();
+
+            string reversedPhrase = ReversWords(phraseInput);
+
+            Console.WriteLine("Инвертированное предложение:");
+            Console.WriteLine(reversedPhrase);
+        }
+        static string[] SplitWords(string phrase)
+        {
+            string[] words = phrase.Split(' ');
+            return words;
+        }
+
+        static string ReversWords(string inputPhrase)
+        {
+            string[] words = SplitWords(inputPhrase);
+            Array.Reverse(words);
+            string reversedPhrase = string.Join(" ", words);
+
+            return reversedPhrase;
+        }
+    }
+}
---
+++ Sentence_Operations/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Sentence_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string sentenceInput = Console.ReadLine();
+
+            string[] words = SplitSentence(sentenceInput);
+
+            PrintWords(words);
+        }
+        static string[] SplitSentence(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            return words;
+        }
+
+        static void PrintWords(string[] words)
+        {
+            Console.WriteLine("Слова в предложении:");
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+
+    }
+}
